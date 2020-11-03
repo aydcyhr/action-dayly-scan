caa8059f7603c6df5228444c1f5d9f3f6e8784fd
aydcyhr <38671092+aydcyhr@users.noreply.github.com>
2020-11-03 16:58:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+import socket
 
 #requests.get('https://sc.ftqq.com/SCU121122T1cc8d68fe0566217f16362970e6a55875f98d59eab0e3.send?text=server'+'&desp=ip%0D%0A%0D%0A%0D%0A'+str(a))
 requests.get('https://sc.ftqq.com/SCU121122T1cc8d68fe0566217f16362970e6a55875f98d59eab0e3.send?text=done'+'&desp='+str(socket.gethostbyname(socket.gethostname()))
