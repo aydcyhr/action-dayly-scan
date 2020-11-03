ae60c2a15cc9e81760b8a654597bed814870e9ec
aydcyhr <38671092+aydcyhr@users.noreply.github.com>
2020-11-03 16:58:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import requests
 import os
-from flask import Flask
-import socket
 
 
 #requests.get('https://sc.ftqq.com/SCU121122T1cc8d68fe0566217f16362970e6a55875f98d59eab0e3.send?text=server'+'&desp=ip%0D%0A%0D%0A%0D%0A'+str(a))
 requests.get('https://sc.ftqq.com/SCU121122T1cc8d68fe0566217f16362970e6a55875f98d59eab0e3.send?text=done'+'&desp='+str(socket.gethostbyname(socket.gethostname()))
-os.system('python -m http.server')
+
 requests.get('https://sc.ftqq.com/SCU121122T1cc8d68fe0566217f16362970e6a55875f98d59eab0e3.send?text=done'+'&desp=ip%0D%0A%0D%0A%0D%0Aflask')
